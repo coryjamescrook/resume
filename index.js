8c38c9761c0baff0c7f86e6df30d0015b6961d79
coryjamescrook <coryjamescrook@gmail.com>
2019-04-13 00:00:37 -0400

    add contact section

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Hero from './Hero'
 import Bio from './Bio'
 import Experience from './Experience'
 import Skills from './Skills'
+import Contact from './Contact'
 
 function Home() {
   return (
@@ -12,6 +13,7 @@ function Home() {
       <Bio bio={resume.bio} />
       <Experience experience={resume.experience} />
       <Skills skills={resume.skills} />
+      <Contact contact={resume.contact} />
     </div>
   )
 }
