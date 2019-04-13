12994ce1b8ffe8e1df224299db368c67cd93bc8b
coryjamescrook <coryjamescrook@gmail.com>
2019-04-13 01:15:38 -0400

    clean up hero

--- Hero.js
+++ Hero.js
@@ -3,7 +3,7 @@ const Hero = ({ hero }) => {
     <section id="hero">
       <div className="hero__content">
         <h1 className="hero__heading">{hero.heading}</h1>
-        { hero.subHeading ? <h2 className="hero__sub-heading">{hero.subHeading}</h2> : null }
+        { hero.subHeading ? <h2 className="hero__sub-heading">{`{ ${hero.subHeading} }`}</h2> : null }
       </div>
     </section>
   )
