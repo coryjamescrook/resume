c04c77ead4c1ee32044677f225ff8014b544b813
coryjamescrook <coryjamescrook@gmail.com>
2019-04-13 00:58:30 -0400

    tightening up styles

--- Bio.js
+++ Bio.js
@@ -5,7 +5,7 @@ const Bio = ({ bio }) => {
     <section id="bio">
       <h1 className="bio__heading">{bio.heading}</h1>
       { bio.subHeading ? <h2 className="bio__sub-heading">{bio.subHeading}</h2> : null }
-      <img className="bio__img" src={profilePhoto} alt="Cory Crook - Profile Photo" />
+      <img className="bio__img u-round" src={profilePhoto} alt="Cory Crook - Profile Photo" />
       <div className="bio__blurb" dangerouslySetInnerHTML={{ __html: bio.blurb }} />
     </section>
   )
