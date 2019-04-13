7afb6f48e1938bfdc5ab15a57ec4a58e0d2e7b58
coryjamescrook <coryjamescrook@gmail.com>
2019-04-12 22:25:18 -0400

    build out most content and start on structural skeleton

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+import resume from '../content'
+
+import Hero from './Hero'
+
 function Home() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <div id="resume">
+      <Hero heading={resume.hero.heading} subHeading={resume.hero.subHeading} />
+    </div>
+  )
 }
 
 export default Home
