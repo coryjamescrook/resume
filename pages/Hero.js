3a3004bf2c21f31136eccd2ff1f66d56c9eeab94
coryjamescrook <coryjamescrook@gmail.com>
2019-04-13 01:06:51 -0400

    vertically align hero section

--- pages/Hero.js
+++ pages/Hero.js
@@ -1,8 +1,10 @@
 const Hero = ({ hero }) => {
   return (
     <section id="hero">
-      <h1 className="hero__heading">{hero.heading}</h1>
-      { hero.subHeading ? <h2 className="hero__sub-heading">{hero.subHeading}</h2> : null }
+      <div className="hero__content">
+        <h1 className="hero__heading">{hero.heading}</h1>
+        { hero.subHeading ? <h2 className="hero__sub-heading">{hero.subHeading}</h2> : null }
+      </div>
     </section>
   )
 }
