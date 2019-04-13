e6b4a84308f6ea44d0fcc04fcfe74661d16a05d7
coryjamescrook <coryjamescrook@gmail.com>
2019-04-13 00:06:18 -0400

    add classnames on all pages

--- pages/Bio.js
+++ pages/Bio.js
@@ -3,8 +3,8 @@ import profilePhoto from '../img/cory-greece.jpeg'
 const Bio = ({ bio }) => {
   return (
     <div id="bio">
-      <h1>{bio.heading}</h1>
-      { bio.subHeading ? <h2>{bio.subHeading}</h2> : null }
+      <h1 className="bio__heading">{bio.heading}</h1>
+      { bio.subHeading ? <h2 className="bio__sub-heading">{bio.subHeading}</h2> : null }
       <img className="bio__img" src={profilePhoto} alt="Cory Crook - Profile Photo" />
       <div className="bio__blurb" dangerouslySetInnerHTML={{ __html: bio.blurb }} />
     </div>
--- pages/Hero.js
+++ pages/Hero.js
@@ -1,8 +1,8 @@
 const Hero = ({ hero }) => {
   return (
     <div id="hero">
-      <h1>{hero.heading}</h1>
-      { hero.subHeading ? <h2>{hero.subHeading}</h2> : null }
+      <h1 className="hero__heading">{hero.heading}</h1>
+      { hero.subHeading ? <h2 className="hero__sub-heading">{hero.subHeading}</h2> : null }
     </div>
   )
 }
