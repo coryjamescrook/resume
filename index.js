3529773107823dde802d328d5a57fc6f0177eb19
coryjamescrook <coryjamescrook@gmail.com>
2019-04-12 22:36:50 -0400

    add bio section

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import resume from '../content'
 
 import Hero from './Hero'
+import Bio from './Bio'
 
 function Home() {
   return (
     <div id="resume">
       <Hero heading={resume.hero.heading} subHeading={resume.hero.subHeading} />
+      <Bio
+        heading={resume.bio.heading}
+        subHeading={resume.bio.subHeading}
+        image={resume.bio.image}
+        blurb={resume.bio.blurb}
+      />
     </div>
   )
 }
