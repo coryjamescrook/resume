0d57589bc11d5deb66a11257e97846dc4445b359
coryjamescrook <coryjamescrook@gmail.com>
2019-04-14 08:10:44 -0400

    use star rating for skills

--- Experience.js
+++ Experience.js
@@ -47,24 +47,12 @@ const Experience = ({ experience }) => {
 
     return c
   }
-  // const jobCards = experience.jobs.map((job, i) => {
-  //   return (
-  //     <div className="experience__job card u-rounded one-half column" key={i}>
-  //       <h3 className="experience__job-company">{job.company}</h3>
-  //       <h4 className="experience__job-title">{job.title}</h4>
-  //       <h5 className="experience__job-dates">{job.dates}</h5>
-  //       <ul className="experience__job-duties">
-  //         { job.duties.map((d, i) => <li key={i} className="experience__job-duty">{d}</li>) }
-  //       </ul>
-  //     </div>
-  //   )
-  // })
 
   return (
     <section id="experience">
       <h1 className="experience__heading u-upcase">{experience.heading}</h1>
       { experience.subHeading ? <h2 className="experience__sub-heading u-lowercase">{experience.subHeading}</h2> : null }
-      <div className="experience__jobs">
+      <div className="experience__jobs u-margin-top-4">
         { jobCards().map(c => c) }
       </div>
     </section>
--- Skills.js
+++ Skills.js
@@ -1,13 +1,25 @@
 const Skills = ({ skills }) => {
+  const createStarRatingClassName = (rating) => {
+    const mapping = {
+      1: 'one-star',
+      2: 'two-stars',
+      3: 'three-stars',
+      4: 'four-stars',
+      5: 'five-stars'
+    }
+
+    return mapping[rating] || ''
+  }
+  
   return (
     <section id="skills">
       <h1 className="skills__heading u-upcase">{skills.heading}</h1>
       {skills.subHeading ? <h2 className="skills__sub-heading u-lowercase">{skills.subHeading}</h2> : null }
       <div className="skills__stack-list">
         { skills.stack.map((s, i) => (
-          <div key={i} className="skills__stack-item skill-card">
+          <div key={i} className="skills__stack-item skill-card u-margin-top-4">
             <h3>{s.skill}</h3>
-            <h4>{`${s.level} / 5`}</h4>
+            <div className={`${createStarRatingClassName(s.level)} rating`}/>
           </div>
         )) }
       </div>
