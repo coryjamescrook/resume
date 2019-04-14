d24a1cd207fbdffb02f851aa5cffc70f8cd1dd2a
coryjamescrook <coryjamescrook@gmail.com>
2019-04-14 08:10:44 -0400

    use star rating for skills

--- pages/Bio.js
+++ pages/Bio.js
@@ -5,8 +5,10 @@ const Bio = ({ bio }) => {
     <section id="bio">
       <h1 className="bio__heading u-upcase">{bio.heading}</h1>
       {bio.subHeading ? <h2 className="bio__sub-heading u-lowercase">{bio.subHeading}</h2> : null }
-      <img className="bio__img u-round" src={profilePhoto} alt="Cory Crook - Profile Photo" />
-      <div className="bio__blurb" dangerouslySetInnerHTML={{ __html: bio.blurb }} />
+      <div className="bio__content u-margin-top-4">
+        <img className="bio__img u-round" src={profilePhoto} alt="Cory Crook - Profile Photo" />
+        <div className="bio__blurb" dangerouslySetInnerHTML={{ __html: bio.blurb }} />
+      </div>
     </section>
   )
 }
